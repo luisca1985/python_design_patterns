47b952b18bbb9874e73c86d27981c74ef863b608
Luis Camilo Jimenez Alvarez <luisca1985@gmail.com>
2022-01-24 21:45:35 -0500

    abstract factory method

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 ## Factory Method
 Factory Method is a Creational Design Pattern that allows an interface or a class to create an object, but lets subclasses decide which class or object to instantiate. Using the Factory method, we have the best ways to create an object. Here, objects are created without exposing the logic to the client, and for creating the new type of object, the client uses the same common interface.
 
+## Abstract Factory Method
+Abstract Factory Method is a Creational Design pattern that allows you to produce the families of related objects without specifying their concrete classes. Using the abstract factory method, we have the easiest ways to produce a similar type of many objects.
+
+It provides a way to encapsulate a group of individual factories. Basically, here we try to abstract the creation of the objects depending on the logic, business, platform choice, etc.
 
 ## References
 - [Python Design Patterns](https://www.geeksforgeeks.org/python-design-patterns/)
---
+++ abstract_factory.py
@@ -0,0 +1,70 @@
+# Python Code for object
+# oriented concepts using
+# the abstract factory
+# design pattern
+
+import random
+
+
+class Course:
+
+    """Courses"""
+
+    def __init__(self, courses_factory=None):
+        """ Course factory is out abstract factory"""
+
+        self.course_factory = courses_factory
+
+    def show_course(self):
+        """creates and shows courses using the abstract factory"""
+
+        course = self.course_factory()
+
+        print(f'We have a course named { course }')
+        print(f'its price is { course.Fee() }')
+
+
+class DSA:
+
+    """Class for Data Structure and Algorithms"""
+
+    def Fee(self):
+        return 11000
+
+    def __str__(self):
+        return "DSA"
+
+
+class STL:
+
+    """Class for Standard Template Library"""
+
+    def Fee(self):
+        return 8000
+
+    def __str__(self):
+        return "STL"
+
+
+class SDE:
+
+    """Class for Software Development Engineer"""
+
+    def Fee(self):
+        return 1500
+
+    def __str__(self):
+        return "SDE"
+
+
+def random_course():
+    """A random class for choosing the course"""
+
+    return random.choice([SDE, STL, DSA])()
+
+
+if __name__ == "__main__":
+    course = Course(random_course)
+
+    for i in range(5):
+        course.show_course()
