4636c9e8f34f6790ba8fda033b4a65ade820b331
Luis Camilo Jimenez Alvarez <luisca1985@gmail.com>
2022-01-24 23:32:32 -0500

    prototype: without using the method

---
+++ prototype.py
@@ -0,0 +1,42 @@
+# concrete course
+class DSA():
+	"""Class for Data Structures and Algorithms"""
+
+	def Type(self):
+		return "Data Structures and Algorithms"
+
+	def __str__(self):
+		return "DSA"
+
+
+# concrete course
+class SDE():
+	"""Class for Software development Engineer"""
+
+	def Type(self):
+		return "Software Development Engineer"
+
+	def __str__(self):
+		return "SDE"
+
+
+# concrete course
+class STL():
+	"""class for Standard Template Library of C++"""
+
+	def Type(self):
+		return "Standard Template Library"
+
+	def __str__(self):
+		return "STL"
+
+
+# main method
+if __name__ == "__main__":
+	sde = SDE() # object for SDE
+	dsa = DSA() # object for DSA
+	stl = STL() # object for STL
+
+	print(f'Name of Course: {sde} and its type: {sde.Type()}')
+	print(f'Name of Course: {stl} and its type: {stl.Type()}')
+	print(f'Name of Course: {dsa} and its type: {dsa.Type()}')
